2a95d8970895313ad3fc0807ca27eb0887aab957
Anhad Singh Arora <anhadarora@gmail.com>
2025-11-06 06:55:28 +0530

    refactor: Rename project from swaha to svaha

---
+++ svaha/android/app/src/main/kotlin/com/scovasolutions/svaha/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.scovasolutions.svaha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- swaha/android/app/src/main/kotlin/com/scovasolutions/swaha/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.scovasolutions.swaha
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
